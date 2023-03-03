b5832ba33b88952f7c00580c83653221732f90a5
Vikash Kumar Singh <mmvvsingh26@gmail.com>
2023-03-03 11:00:09 +0530

    test error resolved

--- src/test/java/com/example/pdfgenerator/controller/PdfControllerTest.java
+++ src/test/java/com/example/pdfgenerator/controller/PdfControllerTest.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
